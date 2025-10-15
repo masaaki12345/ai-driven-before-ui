2ff2ed578c7d86048eadff601040cc3f66897fde
masaaki12345i <m.i.hanaceleb@gmail.com>
2025-10-15 13:51:59 +0900

    clerk add

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { SignIn } from "@clerk/nextjs";
 
-export default function Home() {
+export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen">
-      Hello World
+    <SignIn />
     </div>
   );
 }
